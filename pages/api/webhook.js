745408ec93df87ac1a8aedd4b546c9cc7e08569c
xbornid <36538144+xbornid@users.noreply.github.com>
2025-05-05 23:59:05 +0800

    Update webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,34 +1,24 @@
 export default async function handler(req, res) {
   const { trustedData, untrustedData } = req.body;
-  const action = untrustedData?.buttonIndex; // tombol ditekan user
-
+  const action = untrustedData?.buttonIndex;
   const fid = trustedData?.fid;
   const creator = untrustedData?.inputText || "unknown";
 
   console.log("Webhook triggered:", { fid, action, creator });
 
   switch (action) {
-    case 1: // Buy
-      // Contoh logika buy
+    case 1:
       console.log(`User ${fid} wants to BUY token from ${creator}`);
-      // TODO: Integrasikan dengan smart contract atau API Zora
       break;
-
-    case 2: // Sell
+    case 2:
       console.log(`User ${fid} wants to SELL token from ${creator}`);
-      // TODO: Integrasikan dengan smart contract atau API Zora
       break;
-
-    case 3: // Watch
+    case 3:
       console.log(`User ${fid} is now WATCHING ${creator}`);
-      // TODO: Simpan ke database atau file siapa yang dipantau oleh siapa
       break;
-
-    case 4: // Unwatch
+    case 4:
       console.log(`User ${fid} has UNWATCHED ${creator}`);
-      // TODO: Hapus dari daftar pantauan user
       break;
-
     default:
       console.log("Unknown action");
   }
