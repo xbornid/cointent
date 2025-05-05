306c5ce959c0e5ca2fcffd2819f14bb366079af8
xbornid <36538144+xbornid@users.noreply.github.com>
2025-05-05 10:33:26 +0800

    Create _document.js

---
+++ pages/api/_document.js
@@ -0,0 +1,14 @@
+<meta
+  name="fc:frame"
+  content='{
+    "version": "1",
+    "imageUrl": "https://cointent-xi.vercel.app/og-image.png",
+    "button": {
+      "title": "Open Cointent Tracker",
+      "action": {
+        "type": "post",
+        "url": "https://cointent-xi.vercel.app/api/webhook"
+      }
+    }
+  }'
+/>
